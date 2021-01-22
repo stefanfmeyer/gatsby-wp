a62c54ba5309b1e63bd8c5f9efdf51e8a88f3de6
stefanfmeyer <stefanfmeyer@gmail.com>
2021-01-22 18:07:35 +0000

    Fix index.js

--- src/templates/page/index.js
+++ src/templates/page/index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-// src/templates/page/index.js
-// Reverted to ef4bba22bf39c11e74e05590855afe3dd398db58
-
-import React  from "react"
-=======
 import React from "react"
->>>>>>> d5a56cbba82f6b82ebfa7a1c2ffcf5b7a86782b2
 
 import Layout from "../../components/Layout"
 import SEO from "../../components/SEO"
