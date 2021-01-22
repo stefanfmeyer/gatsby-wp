8e2807635f1129ad88e935bc8cc70197f51af882
stefanfmeyer <stefanfmeyer@gmail.com>
2021-01-22 15:12:52 +0000

    Add .js to imports

--- src/templates/post/index.js
+++ src/templates/post/index.js
@@ -2,8 +2,8 @@
 
 import React  from "react"
 
-import Layout from "../../components/layout"
-import SEO from "../../components/seo"
+import Layout from "../../components/layout.js"
+import SEO from "../../components/seo.js"
 
 
 const Post = ({ pageContext }) => {
