518aa77cf9bea422527a013685184bb0f477c2b0
stefanfmeyer <stefanfmeyer@gmail.com>
2021-01-23 23:25:07 +0000

    ChangeupWorking

---
+++ gatsby-node.js
@@ -0,0 +1,18 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const createPages = require("./create/createPages")
+const createPosts = require("./create/createPosts")
+
+exports.createPagesStatefully = async (
+  { graphql, actions, reporter },
+  options
+) => {
+  await createPages({ actions, graphql, reporter }, options)
+  await createPosts({ actions, graphql, reporter }, options)
+}
